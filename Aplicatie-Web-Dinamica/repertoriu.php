6db3249010ebfd098adac3706b0bb536ca8b8cf2
Nicolaescu Ovidiu-Constantin <86914229+MindOfBear@users.noreply.github.com>
2023-05-05 14:55:26 +0300

    Functionalitate reprtoriu terminata
    Terminata interfata pagini repertoriu pentru utilizator si functionalitatea acesteia

--- Aplicatie-Web-Dinamica/repertoriu.php
+++ Aplicatie-Web-Dinamica/repertoriu.php
@@ -1,5 +1,7 @@
 <?php
+session_start();
 include "header.html";
+require_once "connect.php";
 ?>
 <p class = "currentPage">Repertoriu</p>
 
@@ -7,14 +9,37 @@ include "header.html";
     <h1 style = "text-align: center; color: white;">Repertoriu</h1>
     <h1 class = "infoPiesaRepertoriu">Repertoriul cuprinde</h1>
     <div class= "holderRepertorii">
-        <div class="repertoriu" id = "clickRepertoriu">
-            <p>Nume Repertoriu</p>
-            <button>Show Info</button>
-        </div>
+    <?php 
+        $stmt = $connect->prepare("SELECT * FROM repertoriu");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $rowCounter = $result->num_rows;        
+        while($row = $result->fetch_assoc()) {        
+    ?>
+        <form method = "POST" action = "repertoriu.php">
+            <div class="repertoriu" id = "clickRepertoriu">
+                <p id = "pRepert"><?php echo htmlspecialchars($row['nume']);?></p>
+                <button name = "markRepertoriu" value = "<?php echo htmlspecialchars($row['id']);?>">Show info</button>
+            </div>
+        </form>
+    <?php }?> 
 
     </div>
     <div class = "holderInfoPiese">
-    <p>Capra cu trei iezi - de IL.Caragiale</p>
+    <?php
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $idRepert = $_POST["markRepertoriu"];
+            $stmt = $connect->prepare("SELECT * FROM piesa WHERE id_repertoriu = (?)");
+            $stmt->bind_param("i", $idRepert);
+            $stmt->execute();
+            $result = $stmt->get_result();
+            $rowCounter = $result->num_rows;        
+            while($row = $result->fetch_assoc()) { 
+            
+        ?>
+            <p><?php echo htmlspecialchars($row['nume']);?></p>
+
+    <?php }}?>
     
 
     </div>
